60a193577563ce4da944eccfb2c914c45f2b40b9
2046 <g@ggg.gd>
2017-01-10 14:49:15 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,17 @@
+<style src="./style.css"></style>
+<template src="./template.html"></template>
+
+<script>
+    export default {
+        props: {
+            barHeight: {
+                type: Number,
+                default: 5
+            },
+            value: {
+                type: [Number, String],
+                required: true
+            }
+        }
+    }
+</script>
